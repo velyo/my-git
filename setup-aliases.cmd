1d9d9be32d733c9736cf604540912b42bc2f329b
Velio Ivanov <velio.ivanov@gmail.com>
2017-04-23 15:58:10 +0300

    fix string values

--- setup-aliases.cmd
+++ setup-aliases.cmd
@@ -10,25 +10,25 @@ REM create branch shortcut [usage: git br BRANCH]
 git config --global alias.br branch
 
 REM create a new branch and switch to it [usage: git cb BB-0000]
-git config --global alias.cb “checkout -b”
+git config --global alias.cb "checkout -b"
 
 REM add and commit all changes with comment [usage: git ci “BB-0000 issue”]
-git config --global alias.ci '!git add -A && git commit -m'
+git config --global alias.ci "!git add -A && git commit -m"
 
 REM add changes to last commit [usage: git append]
 git config --global alias.append "!git add -A ; git commit --amend --no-edit -C HEAD"
 
 REM push a new local branch to the upstream [usage: git put BB-0000]
-git config --global alias.put “push --set-upstream origin”
+git config --global alias.put "push --set-upstream origin"
 
 REM rollback the last commit [usage: git rollback]
-git config --global alias.rollback “reset --soft HEAD~”
+git config --global alias.rollback "reset --soft HEAD~"
 
 REM decorated log [usage: git lga]
 git config --global alias.lga "log --graph --oneline --all --decorate"
 
 REM uncommit the last commit [usage: git uncommit]
-git config --global alias.uncommit “reset HEAD~”
+git config --global alias.uncommit "reset HEAD~"
 
 REM unstage the added changes [usage: git unstage]
-git config --global alias.unstage 'reset HEAD --'
+git config --global alias.unstage "reset HEAD --"
